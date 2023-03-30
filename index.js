eccfe68c7b9ab0960184ed3a58485199c551620a
Ruperth <ruperthnyagesoa@gmail.com>
2023-03-30 04:28:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+ for (let i = 0; i < array.length; i++){
+  const num1 = target - array[i];
+
+  for (let x = i + 1; x < array.length; x++) {
+      if (array[x] === num1)
+      return true
+  }
+ }
+ return false;
 }
 
 /* 
@@ -8,7 +17,9 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
-*/
+*/ // the function should
+  //return true if the sum of any two numbers in the array matches the target number
+  //else it should return false.
 
 /*
   Add written explanation of your solution here
@@ -24,6 +35,11 @@ if (require.main === module) {
 
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([3, 5, 7, 9], 11));
 
   console.log("");
 
